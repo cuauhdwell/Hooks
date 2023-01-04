9c073a7ab5cebf98fc31c9f821360cd5782d5f22
Cuauhtémoc Ledesma <cuauhtemoc@gounion.com>
2023-01-03 21:37:53 -0600

    Refactor Promises app

--- src/promises/store/action-creators.ts
+++ src/promises/store/action-creators.ts
@@ -1,14 +1,16 @@
-import { Action, ActionType } from "@/types";
+import axios from "axios";
+import { ActionType } from "@/types";
 
 export default {
-  getFact: (): Action => ({
-    CALL_API: {
-      types: [
-        ActionType.GET_FACT_REQUEST,
-        ActionType.GET_FACT_SUCCESS,
-        ActionType.GET_FACT_FAILURE
-      ],
-      promise: client => client.get('https://uselessfacts.jsph.pl/random.json?language=en'),
-    },
-  }),
+  getFact: () => axios.get('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then((result) => ({
+      CALL_API: {
+        types: [
+          ActionType.GET_FACT_REQUEST,
+          ActionType.GET_FACT_SUCCESS,
+          ActionType.GET_FACT_FAILURE
+        ],
+        result,
+      },
+    }))
 };
--- src/promises/store/middleware.ts
+++ src/promises/store/middleware.ts
@@ -1,36 +1,24 @@
-import axios from 'axios';
 import { Dispatch } from 'redux';
 
-export default () => ({ dispatch, getState }: any) => (next: Dispatch<void>) => (action: any): any => {
-  if (typeof action === 'function') {
-    return action(dispatch, getState);
-  }
+export default () => ({ dispatch, getState }: any) => (next: Dispatch<void>) => (action: any): any =>
+  action.then((resolved) => {
+    if (typeof resolved === 'function') {
+      return resolved(dispatch, getState);
+    }
 
-  const callAPIAction = action['CALL_API'];
-  if (typeof callAPIAction === 'undefined' || !callAPIAction.promise) {
-    return next(action);
-  }
+    const callAPIAction = resolved['CALL_API'];
+    if (typeof callAPIAction === 'undefined') {
+      return next(resolved);
+    }
 
-  const { promise, types, successCB, failureCB, ...rest } = callAPIAction;
-  const [REQUEST, SUCCESS, FAILURE] = types;
+    const { types, result, ...rest } = callAPIAction;
+    const [REQUEST, SUCCESS, FAILURE] = types;
 
-  next({ ...rest, type: REQUEST });
-  return promise(axios.create(), dispatch)
-    .then(
-      (result) => {
-        if (successCB) successCB(result);
-        setTimeout(() => Promise.resolve(), 500);
-        return next({ ...rest, result, type: SUCCESS });
-      },
-      (error) => {
-        if (failureCB) failureCB(error.response.data);
-        if (error.message === 'Operation canceled due to new request.') {
-          Object.assign(error, { response: { status: 429 } });
-          next({ ...rest, error, type: FAILURE });
-        }
-        next({ ...rest, error, type: FAILURE });
-        const authError = error.response && error.response.status === 401;
-        return authError ? null : Promise.reject(error);
-      },
-    );
-};
+    next({ ...rest, type: REQUEST });
+
+    if (result.status === 200) {
+      return Promise.resolve(next({...rest, type: SUCCESS, result}));
+    } else {
+      return Promise.resolve(next({...rest, type: FAILURE, result}));
+    }
+  });
